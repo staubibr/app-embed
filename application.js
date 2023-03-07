a827e5653fe5f7eb5635454465b4360fb17d7e4b
staubibr <staubin.bruno@gmail.com>
2023-03-06 19:39:22 -0500

    Removed WDSV and main. It all happens in applciation.js now

--- application.js
+++ application.js
@@ -1,48 +1,55 @@
-
 'use strict';
 
 import Application from '../app-framework/base/application.js';
 import Core from '../app-framework/tools/core.js';
 import Dom from '../app-framework/tools/dom.js';
+import Net from '../app-framework/tools/net.js';
 import Configuration from '../app-framework/data_structures/visualization/configuration.js';
 import Recorder from '../app-framework/components/recorder.js';
 import wDiagram from '../app-framework/widgets/diagram/w-diagram.js'
 import wGrid from '../app-framework/widgets/grid/w-grid.js'
 import wGIS from '../app-framework/widgets/gis/w-gis.js'
 import Playback from '../app-framework/widgets/playback.js';
+import Loader from '../app-framework/widgets/loader.js';
 
 export default class AppEmbed extends Application { 
 
-	constructor(container, simulation, config, files) {		
-		super(container);
+	async load() {
+		var qry = new URLSearchParams(window.location.search);
 		
-		this.simulation = simulation;
-		this.config = config;
-		this.files = files;
+		this.elems.loader.on("ready", ev => this.initialize(ev.simulation, ev.viz, ev.files));
+		this.elems.loader.on("error", ev => this.handle_error(ev.error));
 		
-		this.initialize();
+		if (qry.get("viz")) {
+			var file = await Net.file(qry.get("viz"), "visualization.json");
+			
+			this.elems.loader.load([file]);
+		}
+		
+		else Dom.remove_css(this.elems.loader.root, "hidden");
 	}
 	
-	async initialize() {
-		this.view = await this.show_view(this.elems.view);
+	async initialize(simulation, config, files) {
+		var view = await this.show_view(simulation, config, files);
 		
-		this.elems.playback.recorder = new Recorder(this.view.canvas);
-		this.elems.playback.initialize(this.simulation, this.config);
+		this.elems.playback.recorder = new Recorder(view.canvas);
+		this.elems.playback.initialize(simulation, config);
 	}
 	
-	show_view(container) {
+	show_view(simulation, config, files) {
 		var d = Core.defer();
 		
-		Dom.add_css(document.body, this.config.type);
+		Dom.remove_css(this.elems.playback.root, "hidden");
+		Dom.add_css(document.body, config.type);
 		
-		if (this.config.type == "diagram") {			
-			return d.Resolved(new wDiagram(container, this.simulation, this.config));
+		if (config.type == "diagram") {			
+			return d.Resolved(new wDiagram(this.elems.view, simulation, config));
 		}
-		else if (this.config.type === "grid") {
-			return d.Resolved(new wGrid(container, this.simulation, this.config));
+		else if (config.type === "grid") {
+			return d.Resolved(new wGrid(this.elems.view, simulation, config));
 		}
-		else if (this.config.type === "gis") {
-			var view = new wGIS(container, this.simulation, this.config, this.files || []);
+		else if (config.type === "gis") {
+			var view = new wGIS(this.elems.view, simulation, config, files || []);
 			
 			view.on("ready", ev => d.Resolve(view));
 			
@@ -51,12 +58,13 @@ export default class AppEmbed extends Application {
 		else {
 			this.handle_error(new Error("The embedded DEVS viewer does not support visualization type " + view));
 		}		
-	}					   
+	}	
 	
 	html() {
 		return	"<main handle='main' class='view-container'>" +
+					"<div handle='loader' class='loader-widget hidden' widget='Api.Widget.Loader'></div>" + 
 					"<div handle='view' class='view'></div>" +
-					"<div handle='playback' widget='Api.Widget.Playback'></div>" +
+					"<div handle='playback' class='playback hidden' widget='Api.Widget.Playback'></div>" +
 				"</main>";
 	}
 }
